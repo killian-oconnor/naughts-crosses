fca38e5cabbc798bd4270214dfe46c13aa82ff86
killian-oconnor <80956433+killian-oconnor@users.noreply.github.com>
2021-12-02 18:05:44 +0000

    first commit of naughts-crosses; initial frontend and js logic testing

---
+++ naughtscrosses.js
@@ -0,0 +1,61 @@
+// Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
+
+// const gameBoard = (() => {
+//     const b = "";
+//     let gameBoard = [b,b,b,b,b,b,b,b,b];
+
+//     return {
+//         gameBoard,
+//     };
+// })();
+
+// const gameFlow = (() => {
+//     let gameBoard = [b,b,b,b,b,b,b,b,b];
+
+//     return {
+//         gameBoard,
+//     };
+// })();
+
+// const Player = (name) => {
+//     const sayName = () => console.log(`my name is ${name}`)
+//     return {sayName}
+// }
+
+// document.getElementById("box1").addEventListener("click", clickO);
+
+
+// function clickO() {
+//     console.log("Running clickO");
+//     console.log(document.getElementById("box1").textContent);
+    
+
+//     if (document.getElementById("box1").textContent.includes("X")) {
+//         document.getElementById("box1").textContent = "O";
+//     }
+//     else if (document.getElementById("box1").textContent.includes("O")) {
+//         document.getElementById("box1").textContent = "X";
+//     }
+    
+// }
+
+const columns = document.getElementsByClassName("col");
+
+Array.from(columns).forEach(element => {
+    element.addEventListener("click", clickOO);
+});
+
+function clickOO() {
+    console.log("Running clickOO");
+    console.log(this.textContent);
+    
+
+    if (this.textContent.includes("X")) {
+        this.textContent = "O";
+    }
+    else if (this.textContent.includes("O")) {
+        this.textContent = "X";
+    }
+    
+}
+
